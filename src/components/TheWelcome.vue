c365050d8176911fe900fd8d1c81f562271db6a6
Jeremy Campbell <jcampbell@arcbtech.com>
2024-07-23 15:19:39 -0400

    Get formatter set up

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -32,7 +32,9 @@ import SupportIcon from './icons/IconSupport.vue';
     <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
     you need to test your components and web pages, check out
     <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener">Cypress Component Testing</a>.
+    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
+      >Cypress Component Testing</a
+    >.
 
     <br />
 
@@ -44,6 +46,7 @@ import SupportIcon from './icons/IconSupport.vue';
       <EcosystemIcon />
     </template>
     <template #heading>Ecosystem</template>
+
     Get official tools and libraries for your project:
     <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
     <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
@@ -63,8 +66,9 @@ import SupportIcon from './icons/IconSupport.vue';
     Got stuck? Ask your question on
     <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
     Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener">StackOverflow</a>. You
-    should also subscribe to
+    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
+      >StackOverflow</a
+    >. You should also subscribe to
     <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
     the official
     <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
@@ -76,7 +80,6 @@ import SupportIcon from './icons/IconSupport.vue';
       <SupportIcon />
     </template>
     <template #heading>Support Vue</template>
-
     As an independent project, Vue relies on community backing for its sustainability. You can help
     us by
     <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
