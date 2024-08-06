809d5df65e7475e1ba6504220850c2061f7d34d3
Jeremy Campbell <jcampbell@arcbtech.com>
2024-08-06 13:40:15 -0400

    Remove hard-coded images

--- src/App.vue
+++ src/App.vue
@@ -15,20 +15,6 @@ function addImages(imageUrls) {
   <InflatableCardStage>
     <div class="photo-cards">
       <AddPhotoCard @add-images="addImages" />
-      <PhotoCard
-        src="https://asset.gecdesigns.com/img/wallpapers/beautiful-fantasy-wallpaper-ultra-hd-wallpaper-4k-sr10012418-1706506236698-cover.webp"
-      />
-      <PhotoCard src="https://images.pexels.com/photos/1624496/pexels-photo-1624496.jpeg" />
-      <PhotoCard src="https://wallpapers.microsoft.design/images/wallpapers.jpg" />
-      <PhotoCard
-        src="https://www.ytechb.com/wp-content/uploads/2022/08/Windows-365-Wallpapers.webp"
-      />
-      <PhotoCard
-        src="https://www.pixelstalk.net/wp-content/uploads/images6/3D-Wallpaper-Desktop.jpg"
-      />
-      <PhotoCard
-        src="https://t3.ftcdn.net/jpg/05/85/86/44/360_F_585864419_kgIYUcDQ0yiLOCo1aRjeu7kRxndcoitz.jpg"
-      />
       <PhotoCard v-for="image in images" :key="image" :src="image" />
     </div>
   </InflatableCardStage>
